844c451540dd827aedc11c2195e0512984082157
jafow <jared.a.fowler@gmail.com>
2018-04-19 18:33:58 -0700

    Adds handler for POST

--- src/components/GeoCode.vue
+++ src/components/GeoCode.vue
@@ -8,7 +8,9 @@
 
                 <label for="city" class="">City, State</label>
                 <input type="text" name="city" id="city" placeholder="Mountain View, CA" class="input-reset mv2" />
+                <button class="btn br-15 dark-green" v-on:click="submit">Submit</button>
             </form>
+            <p class="response-msg">{{response}}</p>
         </section>
     </div>
 </template>
@@ -19,10 +21,47 @@ export default {
   data () {
     return {
       msg: 'GeoCode Page',
-      title: 'GeoCode'
+      title: 'GeoCode',
+      response: null
+    }
+  },
+  methods: {
+    displayMessage: function displayMessage (msg) {
+      if (msg.lat || msg.lon) {
+        this.response = `The latitude is ${msg.lat} and longitude is ${msg.lng}`
+      } else {
+        this.response = `There was an error in this request: ${msg}`
+      }
+      return this.response
+    },
+    submit: function submit () {
+      let self = this
+      let data = new FormData(document.getElementById('form-geocode'))
+      fetch('//localhost:4000/geocode', {
+        method: 'POST',
+        body: data,
+        mode: 'cors'
+      })
+        .then(function onFetchSuccess (res) {
+          if (res.ok) {
+            let json = res.json()
+            return json
+          }
+          return res.error
+        })
+        .then(function handleSuccess (json) {
+          if (json.error) {
+            return self.displayMessage(json.error)
+          }
+          return self.displayMessage(json.location)
+        })
+        .catch(function onFetchErr (err) {
+          return self.displayMessage(err.error)
+        })
     }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
